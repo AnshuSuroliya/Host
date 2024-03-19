52c40a431a77a8f354e0fb593ee3c93343f0df40
Anshu Suroliya <2000108444@hexaware.com>
2024-03-19 17:52:56 +0530

    i

---
+++ projects/host/src/app/GuardServices/is-logged-in.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
+import { JwtService } from '../jwt.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsLoggedInService implements CanActivate {
+
+  constructor(private router:Router ,private jwtHelper:JwtService) { }
+  
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    let valid =false;
+
+    await this.jwtHelper.validateToken(localStorage.getItem("jwtToken")+"").subscribe((res:any)=>{
+        valid=res;
+    })
+
+    if(valid==false){
+      this.router.navigate(["authpage"]);
+    }
+    return valid;
+  }
+
+  
+}
